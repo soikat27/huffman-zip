1bf1c217815f0721fcc0e8c6e4635a6e44542ea3
Soikat <193371447+soikat27@users.noreply.github.com>
2025-10-02 18:15:28 -0400

    Implement HNodeComparator
    -- Add HNodeComparator.java to define comparison logic for HNode objects

---
+++ src/huffman/HNodeComparator.java
@@ -0,0 +1,20 @@
+package huffman;
+
+import java.util.Comparator;
+
+public class HNodeComparator implements Comparator<HNode> {
+    
+    @Override
+    public int compare(HNode a, HNode b) 
+	{
+        int freqCompare = Integer.compare(a.getFrequency(), b.getFrequency());
+        
+        if (freqCompare != 0) 
+        {
+        	return freqCompare;
+        }
+        
+        return a.getSymbols().compareTo(b.getSymbols());
+    }
+
+}
