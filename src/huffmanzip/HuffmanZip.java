fc62e667706b3bb5aae14d6a94e1814bd5495b9c
Soikat <193371447+soikat27@users.noreply.github.com>
2025-10-02 23:15:57 -0400

    Implement decode logic
    -- Implement decode()
    -- Incorporate existing-file handling

--- src/huffmanzip/HuffmanZip.java
+++ src/huffmanzip/HuffmanZip.java
@@ -1,10 +1,14 @@
 package huffmanzip;
 
+import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 import java.util.TreeMap;
 
 import huffman.HuffmanTree;
+import utils.BitInputStream;
 import utils.BitOutputStream;
 
 public class HuffmanZip {
@@ -55,5 +59,51 @@ public class HuffmanZip {
 
 		reader.close();
 		bitOutputStream.close();
+	}
+
+    public static void decode(String fileName) throws IOException, ClassNotFoundException
+	{
+		String decodedFile = fileName.replace(".hz", "");
+
+        File outputFile = new File (decodedFile);
+
+        if (outputFile.exists())
+        {
+            System.out.println ("A file with the name '" + decodedFile + "' already exists. Type: (y / n(or anything else))");
+            System.out.println ("'y': Delete existing file and rewrite");
+            System.out.println ("'n / (anything else)': abort with no action");
+
+            Scanner sc = new Scanner (System.in);
+            String response = sc.next();
+        
+            if (response.equalsIgnoreCase("y"))
+            {
+                outputFile.delete();
+            }
+
+            else
+            {
+                sc.close();
+                return;
+            }
+
+            sc.close();
+        }
+
+		BitInputStream bitInputStream = new BitInputStream(fileName);
+		TreeMap<Character, Integer> frequencies = (TreeMap<Character, Integer>) bitInputStream.readObject();
+
+		HuffmanTree hTree = new HuffmanTree(frequencies);
+
+		FileWriter writer = new FileWriter(decodedFile);
+
+		while (bitInputStream.hasNext())
+		{
+			char curChar = hTree.readCode(bitInputStream);
+			writer.write(curChar);
+		}
+
+		writer.close();
+		bitInputStream.close();
 	}
 }
